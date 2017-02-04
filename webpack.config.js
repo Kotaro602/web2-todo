1e75d2e1504200d496b8baa4c30a2481c2949eb6
KOTARO SEKIHARA <kotaro@KOTAROnoiMac.local>
2017-02-05 01:40:23 +0900

    ビルドできない問題について

--- webpack.config.js
+++ webpack.config.js
@@ -40,10 +40,10 @@ module.exports = {
    },
    resolve: {
       extensions: ["", ".webpack.js", ".web.js", ".js", ".jsx"]
-   }
-   // plugins: [
-   //    new webpack.DefinePlugin({
-   //       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-   //    })
-   // ]
+   },
+   plugins: [
+      new webpack.DefinePlugin({
+         'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      })
+   ]
 };
